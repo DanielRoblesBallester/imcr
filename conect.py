c36d9ea4889778179722d8be49e070506c6402a3
Daniel Robles Ballester <drb37@gcloud.ua.es>
2022-05-05 09:59:21 +0200

    guardado de progreso

--- conect.py
+++ conect.py
@@ -2,6 +2,7 @@ from ipaddress import ip_address
 import socket
 import sys
 import json
+import time
  
 # Set up a TCP/IP server
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,21 +21,33 @@ while True:
     try:
         print("Connected to client IP: {}".format(client))
          
-        # Receive and print data 32 bytes at a time, as long as the client is sending something
+        # Receive and dprint data 32 bytes at a time, as long as the client is sending something
+        data = b""
         while True:
-            data = connection.recv(130)
-            dataJson = json.loads(data)
+            data += connection.recv(92)
+            print("Data", data)
+            dataSplit = data.split(b'\n')
+            print(dataSplit[0])
+            print("Dictionary before parsing: ", dataSplit[0])
+            if dataSplit[0] == b'' or dataSplit[0] == b'}':
+                data = b""
+                continue
+            dataJson = json.loads(dataSplit[0])
              
             # printing all elements of dictionary
-            print("Dictionary after parsing: ", dataJson)
+            #print("Dictionary after parsing: ", dataJson)
 
-            print("Timestamp: ", dataJson['accelerometer']['timestamp'])
-            print("X: ", dataJson['accelerometer']['value'][0])
-            print("Y: ", dataJson['accelerometer']['value'][1])
-            print("G: ", dataJson['accelerometer']['value'][2])
+            #print("Timestamp: ", dataJson['accelerometer']['timestamp'])
+            #print("X: ", dataJson['accelerometer']['value'][0])
+            #print("Y: ", dataJson['accelerometer']['value'][1])
+            #print("G: ", dataJson['accelerometer']['value'][2])
  
-            if not data:
-                break
+            time.sleep(1)
+            data = b""
+            if len(dataSplit) == 2:
+                data += dataSplit[1]
+            #if not data:
+            #    break
  
     finally:
         connection.close()
